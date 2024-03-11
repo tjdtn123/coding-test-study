cf9229305cd00d2cd2cc65f74ecba8c5d9513303
tjdtn123 <99237245+tjdtn123@users.noreply.github.com>
2024-03-11 13:55:05 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        int answer = 0;
+        String[] arr = new String[my_string.length()];
+        for(int i = 0; i < my_string.length(); i++){
+            arr[i] = my_string.substring(i);
+        }
+        for(String elem : arr){
+            if(elem.equals(is_suffix)){
+                answer += 1;
+            }
+        }
+
+        return answer;
+    }
+}
