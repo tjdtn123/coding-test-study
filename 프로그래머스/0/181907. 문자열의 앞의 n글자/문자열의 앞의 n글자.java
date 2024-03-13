1949f2f4420f042737ceee300adb207c3722c623
tjdtn123 <99237245+tjdtn123@users.noreply.github.com>
2024-03-13 13:39:18 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        
+        
+        
+        return my_string.substring(0,n);
+    }
+}
