e8facc04630be0edb1f11ba4e3fba2981ef9e1c7
tjdtn123 <99237245+tjdtn123@users.noreply.github.com>
2024-03-22 16:29:40 +0900

    [level 0] Title: ad 제거하기, Time: 0.71 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] strArr) {
+        String[] answer = {};
+        
+        List<String> list = new ArrayList<>();
+        for(String str : strArr){
+            if(!str.contains("ad")){
+                list.add(str);
+            }
+        }
+        answer = new String[list.size()];
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
